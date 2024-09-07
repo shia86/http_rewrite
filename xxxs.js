e135d285f3168c081cc78e964b3ff3f8db965ec4
shia <zhangyuan1993@gmail.com>
2024-09-07 16:35:58 +0800

    Update xxxs.js

--- xxxs.js
+++ xxxs.js
@@ -1,2 +1,8 @@
-console.log(JSON.stringify($request));
-$done({});
+var url = new URL($request.url);
+var params = new URLSearchParams(url.search);
+
+// params.set('current', '90')
+
+url.search = params.toString();
+var modifiedUrl = url.toString();
+$done({url:$modifiedUrl});
