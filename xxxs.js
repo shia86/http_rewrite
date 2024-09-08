f37340b428d389076d0c120295c288a883c48e72
shia86 <zhangyuan1993@gmail.com>
2024-09-08 14:03:39 +0800

    update

--- xxxs.js
+++
@@ -1,8 +0,0 @@
-var url = new URL($request.url);
-var params = new URLSearchParams(url.search);
-
-// params.set('current', '90')
-
-url.search = params.toString();
-var modifiedUrl = url.toString();
-$done({url : modifiedUrl});
---
+++ xxxs/update.js
@@ -0,0 +1,11 @@
+var body = new URL($request.body);
+var params = new URLSearchParams(body);
+var modifiedBody = body;
+
+if (params.has('current_page')) {
+    params.set('learn_time', '90');
+    params.set('current_time', '90');
+    modifiedBody = params.toString();
+}
+
+$done({body : modifiedBody});
