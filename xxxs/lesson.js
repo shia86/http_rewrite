fe343f8318d573520aee0748452d51ca150fb249
shia86 <zhangyuan1993@gmail.com>
2024-09-08 14:44:03 +0800

    update

--- xxxs/lesson.js
+++ xxxs/lesson.js
@@ -3,5 +3,6 @@ var resp = JSON.parse(body)
 var data = resp['data']
 if (data && data['type'] == 'video') {
     data['learn_time'] = data['length']
+    data['current_time'] = data['length']
 }
 $done(JSON.stringify(resp))
