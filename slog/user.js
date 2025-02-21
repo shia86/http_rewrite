7c73c8564f17bdca1260b7b37358fb3f9e5863fa
shia <zhangyuan1993@gmail.com>
2025-02-21 22:32:34 +0800

    update

--- slog/user.js
+++ slog/user.js
@@ -4,6 +4,8 @@ try {
     console.log(resp['user_roles'])
     resp['user_roles'][0] = "platinum"
     resp['expiration'] = 2055649382000
+    var accounts = resp['account_list']
+    accounts.push(accounts[4])
 } catch (error) {
     console.log(error)
 }
