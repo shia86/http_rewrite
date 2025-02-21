164249d08d43d31b09701e844bf55157739f2244
shia <zhangyuan1993@gmail.com>
2025-02-21 22:12:20 +0800

    udate

--- slog/user.js
+++ slog/user.js
@@ -2,7 +2,7 @@ var body = $response.body
 var resp = JSON.parse(body)
 try {
     console.log(resp['user_roles'])
-    resp['expiration] = 2055649382000
+    resp['expiration'] = 2055649382000
 } catch (error) {
     console.log(error)
 }
