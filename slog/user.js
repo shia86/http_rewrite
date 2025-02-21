7ee0c37b646de447ead224873e4a6613ec47cfc6
shia <zhangyuan1993@gmail.com>
2025-02-21 22:11:41 +0800

    update

--- slog/user.js
+++ slog/user.js
@@ -2,7 +2,6 @@ var body = $response.body
 var resp = JSON.parse(body)
 try {
     console.log(resp['user_roles'])
-    resp['user_roles'][0] = "platinum"
     resp['expiration] = 2055649382000
 } catch (error) {
     console.log(error)
