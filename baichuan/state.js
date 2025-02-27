d32d87a181f4a896bce205ac7ffe4b3eda4c9de6
shia <zhangyuan1993@gmail.com>
2025-02-27 14:00:12 +0000

    update

--- baichuan/state.js
+++ baichuan/state.js
@@ -2,7 +2,7 @@ var body = $response.body
 var resp = JSON.parse(body)
 try {
     resp['Result']['isLifelong'] = 1
-    resp['Result']['vipType'] = 1
+    resp['Result']['vipType'] = 4
     resp['Result']['expiresTime'] = 2055649382000
 } catch (error) {
     console.log(error)
